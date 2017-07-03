c3306d3cf92fea6ecd77d881b5b3341fc069bce9
vuhoanglinh96 <linhvh.96@gmail.com>
2017-07-03 14:13:39 +0700

    Chapter9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,29 @@
 class SessionsController < ApplicationController
+  before_action :find_user, only: [:create]
+
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      redirect_to @user
     else
-      flash.now[:danger] = t ".login_error"
+      flash.now[:danger] = t ".password_error"
       render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+  def find_user
+    @user = User.find_by email: params[:session][:email].downcase
+    return if @user
+    flash.now[:danger] = t ".email_error"
+    render :new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  attr_reader :remember_token
 
   before_save :email_downcase
 
@@ -7,10 +8,40 @@ class User < ApplicationRecord
   validates :email, format: {with: VALID_EMAIL_REGEX},
     presence: true, uniqueness: {case_sensitive: false},
     length: {maximum: Settings.validates.email.maximum}
-  validates :password, presence: true,
+  validates :password, presence: true, allow_nil: true,
     length: {minimum: Settings.validates.password.minimum}
   validates :name, presence: true,
     length: {maximum: Settings.validates.username.maximum}
+
+  class << self
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.blank?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
 
   private
 
